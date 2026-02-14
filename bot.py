279c31dcc26d1efa4c996101eadc0cf310f80faf
xavaris <xavaris@gmail.com>
2026-02-14 21:08:49 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -186,7 +186,7 @@ async def buttons(update:Update,context:ContextTypes.DEFAULT_TYPE):
 
 if q.data=="blacklist_add" and uid==ADMIN_ID:
     context.user_data["add_bl"]=True
-        await q.message.reply_text("✍️ Podaj słowo do DODANIA do blacklisty:")
+       await q.message.reply_text("✍️ Podaj słowo do DODANIA do blacklisty:")
     return
 
 # ================= BLACKLIST REMOVE =================
@@ -364,6 +364,7 @@ def main():
 
 if __name__=="__main__":
     main()
+
 
 
 
